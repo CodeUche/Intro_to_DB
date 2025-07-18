51fb7f1ee4787a77606dbb61bc96f83a0123ec6b
CodeUche <preciousueze@outlook.com>
2025-07-18 17:48:05 +0100

    Created magical tables

--- task_2.sql
+++ task_2.sql
@@ -2,12 +2,12 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
-CREATE TABLE authors(
+CREATE TABLE Authors(
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE books(
+CREATE TABLE Books(
     book_id INT AUTO_INCREMENT PRIMARY KEY, 
     title VARCHAR(130) NOT NULL,
     price DOUBLE, 
@@ -16,21 +16,21 @@ CREATE TABLE books(
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
     );
 
-CREATE TABLE customers(
+CREATE TABLE Customers(
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE orders(
+CREATE TABLE Orders(
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE order_Details(
+CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
